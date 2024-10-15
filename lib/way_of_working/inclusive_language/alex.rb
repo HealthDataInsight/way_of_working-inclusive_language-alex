2132b6f138bfea4bba42cfdf7aa0b458f6a2740c
Tim Gentry <52189+timgentry@users.noreply.github.com>
2024-10-15 14:03:40 +0100

    Fixed paths

--- lib/way_of_working/inclusive_language/alex.rb
+++ lib/way_of_working/inclusive_language/alex.rb
@@ -17,30 +17,32 @@ module WayOfWorking
 end
 
 module WayOfWorking
-  # This class defines the exec (i.e. run) parent command
-  class Exec
-    register(InclusiveLanguage::Alex::Generators::Exec, 'inclusive_language', 'inclusive_language',
-             <<~LONGDESC)
-               Description:
-                   This runs inclusive language tests on this project
+  module SubCommands
+    # This reopens the "way_of_working exec" (i.e. run) sub command
+    class Exec
+      register(InclusiveLanguage::Alex::Generators::Exec, 'inclusive_language', 'inclusive_language',
+              <<~LONGDESC)
+                Description:
+                    This runs inclusive language tests on this project
 
-               Example:
-                   way_of_working exec inclusive_language
-             LONGDESC
-  end
+                Example:
+                    way_of_working exec inclusive_language
+              LONGDESC
+    end
 
-  # This class defines the "init" parent command
-  class Init < SubCommandBase
-    register(InclusiveLanguage::Alex::Generators::Init, 'inclusive_language', 'inclusive_language',
-             <<~LONGDESC)
-               Description:
-                   Installs alex config file into the project
+    # This reopens the "way_of_working init" sub command
+    class Init
+      register(InclusiveLanguage::Alex::Generators::Init, 'inclusive_language', 'inclusive_language',
+              <<~LONGDESC)
+                Description:
+                    Installs alex config file into the project
 
-               Example:
-                   way_of_working init inclusive_language
+                Example:
+                    way_of_working init inclusive_language
 
-                   This will create:
-                       .alexrc
-             LONGDESC
+                    This will create:
+                        .alexrc
+              LONGDESC
+    end
   end
 end
